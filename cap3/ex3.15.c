d2da5ab4fdecbcdc3e78db3375d9753fbf995944
PietroTy <pietro.turcimm@gmail.com>
2024-04-08 17:56:56 -0300

    cap4

---
+++ cap3/ex3.15.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+    int n;
+    int f = 0;
+
+    printf("Numero: ");
+    scanf("%d", &n);
+
+    if ( n >= 0 ) {
+
+        for ( int i = 1; i <= n; i++ ) {
+
+            f = f * i;
+
+        }
+
+        printf("%d! = %d\n", n, f);
+
+    }
+    else {
+
+        printf("Nao ha fatorial de numero negativo.\n");
+
+    }
+    
+    return 0;
+}
---
+++ cap4/ex4.1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main( void ) {
+
+int n [5];
+int c [5];
+
+int t = (int) ( sizeof( n ) / sizeof( n[0] ) );
+
+for ( int i = 0; i < t; i++ ) {
+    printf( "array[%d]: ", i );
+    scanf( "%d", &n[i] );
+    printf( "\n" );
+}
+
+for ( int i = 0; i < t; i++ ) {
+    c[i] = n[i] * n[i] * n[i];
+
+}
+
+for ( int i = 0; i < t; i++ ) {
+    printf( "arrayCubo[%d] = %d", i, c[i] );
+    printf( "\n" );
+}
+
+return 0;
+
+}
