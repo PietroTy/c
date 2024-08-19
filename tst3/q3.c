d4b174e8c0a46202b8b8f81fe2e99d54d7fa7d77
PietroTy <pietro.turcimm@gmail.com>
2024-08-19 17:15:11 -0300

    tst3

--- tst3/q3.c
+++ tst3/q3.c
@@ -1,1 +1,26 @@
 //PietroTy//
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+
+void tornarMinuscula( char *str );
+
+int main(void){
+
+    char string[41];
+
+    printf("Frase: ");
+    fgets( string, 41, stdin );
+    string[strlen(string)-1] = '\0';
+
+    tornarMinuscula(&string);
+    printf("%c", string);
+
+
+}
+
+void tornarMinuscula( char *str ){
+    *str = tolower(str);
+
+}
